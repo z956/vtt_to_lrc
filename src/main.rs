9e1e6dca15d8d6e10e1935a70b4ee2d239b451a4
Li-Wei Cheng <z956@Li-WeideMac-Studio.local>
2026-06-28 00:15:11 +0800

    Add clap dep. Remove cli.rs

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,40 @@
-mod cli;
 mod error;
 mod lrc;
 mod vtt;
+
+use clap::Parser;
 
 use error::ProcessError;
 use lrc::output_to_lrc_file;
 use vtt::parse_vtt_blocks;
 
-fn process(opts: cli::CmdOpts) -> Result<(), ProcessError> {
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct CmdOpts {
+    input_file_name: String,
+    #[arg(short, long, default_value_t)]
+    output_file_name: String,
+}
+
+fn process(opts: CmdOpts) -> Result<(), ProcessError> {
     let blocks = parse_vtt_blocks(&opts.input_file_name)?;
     output_to_lrc_file(&opts.output_file_name, &blocks)
 }
 
 fn main() {
-    let opt = cli::parse_opts();
-    match opt {
-        Err(error) => eprintln!("{}", error),
-        Ok(opts) => {
-            if opts.help {
-                cli::print_help();
-            } else {
-                if let Err(error) = process(opts) {
-                    match error {
-                        ProcessError::Io(e) => eprintln!("{}", e),
-                        ProcessError::Parse(e) => eprintln!("{}", e),
-                    }
-                }
-            }
+    let mut opts = CmdOpts::parse();
+    if opts.output_file_name.is_empty() {
+        if let Some(base_name) = opts.input_file_name.strip_suffix(".vtt") {
+            opts.output_file_name = format!("{}.lrc", base_name);
+        } else {
+            opts.output_file_name = format!("{}.lrc", opts.input_file_name);
+        }
+    }
+
+    if let Err(error) = process(opts) {
+        match error {
+            ProcessError::Io(e) => eprintln!("{}", e),
+            ProcessError::Parse(e) => eprintln!("{}", e),
         }
     }
 }
